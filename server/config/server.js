6d43269bfe2d874a44bdb394091666644b68d5b5
Patrick Morris <patrickjohnmorris@gmail.com>
2020-12-06 21:41:14 -0500

    initial commit next and strapi

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '083de4cf37d385f721771dc18621d276'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '054c1bab-72d9-4c27-8b65-96eb4dc431b4'
+};
